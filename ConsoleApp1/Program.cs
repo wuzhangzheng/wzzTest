6fe100d51272bd98495fb6c686357da0d67b5dbe
WZZ-HOSPITAL <WZZ-HOSPITAL@DESKTOP-92STND0>
2018-06-09 09:56:09 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 4;
+            UpdateRefInt(ref i);
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+
+        private static void UpdateRefInt(ref int i)
+        {
+            i = 8;
+        }
+    }
+}
